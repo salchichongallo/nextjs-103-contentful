7bc0e9e938f7325ecaea9bc6f3e9db1fffbbafab
Jimmy Murillo <salchichongallo1997@gmail.com>
2022-11-29 18:34:28 -0500

    feat(home): list posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,57 @@
-export default function Home() {
+import Link from "next/link";
+import { GetStaticProps } from "next";
+import { BlogPost } from "../lib/BlogPost.interface";
+import { fetchContentful } from "../lib/fetchContentful";
+
+type HomeProps = {
+  posts: BlogPost[];
+};
+
+export default function Home({ posts }: HomeProps) {
   return (
     <div>
-      <div>Home</div>
+      <header>
+        <h1>Blog.</h1>
+      </header>
+      <main>
+        <section>
+          {posts.map((post) => (
+            <article key={post.slug}>
+              <Link href={`/posts/${post.slug}`}>{post.title}</Link>
+              <p>{post.excerpt}</p>
+              <small>{post.publishedAt.slice(0, 10)}</small>
+            </article>
+          ))}
+        </section>
+      </main>
     </div>
   );
 }
+
+const QUERY_LANDING_POSTS = /* GraphQL */ `
+  query QueryLandingPosts {
+    blogPostCollection {
+      total
+      items {
+        title
+        slug
+        publishedAt
+        excerpt
+      }
+    }
+  }
+`;
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const {
+    data: {
+      blogPostCollection: { items: posts },
+    },
+  } = await fetchContentful(QUERY_LANDING_POSTS);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
