ef661c8374f1aa59dbb533f259b24b86bb09b88d
Jimmy Murillo <salchichongallo1997@gmail.com>
2022-11-29 20:16:32 -0500

    feat(contentful): support content preview

--- lib/fetchContentful.ts
+++ lib/fetchContentful.ts
@@ -1,13 +1,28 @@
-export function fetchContentful(query: string, variables: any = {}) {
+type Options = {
+  preview?: boolean;
+};
+
+export function fetchContentful(
+  query: string,
+  variables: any = {},
+  options: Options = {}
+) {
   const fetchUrl = `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`;
 
   const fetchOptions = {
     method: "POST",
     headers: {
-      Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+      Authorization: `Bearer ${
+        options.preview
+          ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
+          : process.env.CONTENTFUL_ACCESS_TOKEN
+      }`,
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ query, variables }),
+    body: JSON.stringify({
+      query,
+      variables: { ...variables, preview: !!options.preview },
+    }),
   };
 
   return fetch(fetchUrl, fetchOptions).then((response) => response.json());
