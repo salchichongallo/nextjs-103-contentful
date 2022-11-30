3cf79436ff95e2a7b00cd0873b5dd2df3f835d71
Jimmy Murillo <salchichongallo1997@gmail.com>
2022-11-29 19:01:16 -0500

    feat(contentful): add support for variables

--- lib/fetchContentful.ts
+++ lib/fetchContentful.ts
@@ -1,4 +1,4 @@
-export function fetchContentful(query: string) {
+export function fetchContentful(query: string, variables: any = {}) {
   const fetchUrl = `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`;
 
   const fetchOptions = {
@@ -7,7 +7,7 @@ export function fetchContentful(query: string) {
       Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ query }),
+    body: JSON.stringify({ query, variables }),
   };
 
   return fetch(fetchUrl, fetchOptions).then((response) => response.json());
