2f62c5558a59543ed69693ae9e357b3a7917ab81
Jimmy Murillo <salchichongallo1997@gmail.com>
2022-12-02 15:17:11 -0500

    feat: add Apollo client

---
+++ lib/apollo-client.ts
@@ -0,0 +1,17 @@
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+
+let uri = `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`;
+if (process.env.CONTENTFUL_ENVIRONMENT) {
+  uri += `/environments/${process.env.CONTENTFUL_ENVIRONMENT}`;
+}
+
+export const client = new ApolloClient({
+  ssrMode: true,
+  link: new HttpLink({
+    uri,
+    headers: {
+      Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+    },
+  }),
+  cache: new InMemoryCache(),
+});
